4d65c2e1637e2838124d457f1d4610b138bf37a2
ashishagrawal0999 <ashishagrawal09990@gmail.com>
2026-09-01 21:49:54 +0530

    Initial commit

---
+++ sliding-window/code.cpp
@@ -0,0 +1,10 @@
+// sliding window -> ek window jiski size dynamic hai , apne mann se expand yaa shrink krskta hai
+
+// Array/String Me Lgega
+// Subaarray -> contiguous part of array
+// Substring -> contiguous part of string
+
+// Subarray -> {2,3,4,5} -> {2,3} , {3,4,5} , {2,3,4,5} , {4} , {5} , {2} , {3}
+
+
+
