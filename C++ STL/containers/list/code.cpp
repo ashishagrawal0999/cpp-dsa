3355bd441e18520aaa8d9fde3b8e18d9a0452682
ashishagrawal0999 <ashishagrawal09990@gmail.com>
2026-09-08 16:01:23 +0530

    updated stl list file

--- C++ STL/containers/list/code.cpp
+++ C++ STL/containers/list/code.cpp
@@ -4,38 +4,33 @@
 using namespace std;
 int main(){
 
-    // List -> a ds in which data is stored in the form of nodes , and has pointer to next node 
+    // List -> a ds in which data is stored in the form of nodes , and has pointer to next node
     // non contagious memory allocation
 
     // header file -> #include<list>
+    // Allows fast insertion and removals anywhere in the list
 
     // Doubly Linked list -> has pointer to previous node , data , and has pointer to next node
 
     // | prev | data | next |->| prev | data | next |->| prev | data | next |
     // head ^                                                          ^null
-    
-
-    // Allows fast insertion and removals anywhere in the list
 
     // In vectors -> to access any data takes O(1) time
     // No random access like vectors -> takes O(n) time to access any data
 
 
-   // Creation
-   list<int> myList;
+    // creation :
+    list<int> myList;
 
-   myList.push_back(10);
-   // 10
-   myList.push_back(20);
-   // 10->20
-   myList.push_back(30);
-
-   
-   // 10->20->30
-
-
-   myList.push_front(40);
-   // 40->10->20->30
+    myList.push_back(10);
+    // 10
+    myList.push_back(20);
+    // 10->20
+    myList.push_back(30);
+    // 10->20->30
+    
+    myList.push_front(40);
+    // 40->10->20->30
     myList.push_front(50);
     // 50->40->10->20->30
 
@@ -45,56 +40,47 @@ int main(){
     myList.pop_front();
     // 40->10->20
 
-    cout << myList.size() << " ";
-
-   
+    cout << myList.size() << " " << endl;
 
     // using for each loop
-
     for (int vals : myList){
-        cout<<vals<<" ";
+        cout << vals << " ";
     }
+    cout << endl;
 
-    cout<<endl;
-
+    // empty() :
     if (myList.empty()){
-        cout << "List is empty"<<endl;
+        cout << "List is empty" << endl;
+    } else {
+        cout << "List is not empty" << endl;
     }
-    else{
-        cout << "List is not empty"<<endl;
-    }
 
-    cout<<myList.front()<<" ";
-    cout<<myList.back()<<" ";
+    cout << "Front : " << myList.front() << " " << endl; // 40
+    cout << "Back : " << myList.back() << " " << endl;   // 20
+    myList.push_back(50);                                // 40->10->20->50
 
-    cout<<endl;
+    cout << "before removing" << endl;
+    list<int>::iterator it = myList.begin();
 
-    myList.push_back(10);
-
-
-    cout<<"before removing"<<endl;
-    list<int> :: iterator it = myList.begin();
-
-    while(it != myList.end()){
-        cout<<*(it)<<" ";
+    while (it != myList.end()){
+        cout << *(it) << " ";
         it++;
     }
+    cout << endl;
 
-    cout<<endl;
+    // remove() :
+    // removing element
+    myList.remove(10);                                  // 40->20->50
 
-    myList.remove(10);
+    list<int>::iterator it2 = myList.begin(); // naya iterator banana hoga yaa iterator reset krna hoga because , jab humne pehle while loop use kiya tab itertor list.end() ko point krrha tha traversal complete hone ke baad
 
-    list<int> :: iterator it2;      // naya iterator banana hoga yaa iterator reset krna hoga because , jab humne pehle while loop use kiya tab itertor muList.end() ko point krrha tha traversal complete hone ke baad
-
-    cout<<"after removing"<<endl;
-    while(it2 != myList.end()){
-        cout<<*(it2)<<" ";
-        it++;
+    cout << "after removing" << endl;
+    while (it2 != myList.end()){
+        cout << *(it2) << " ";
+        it2++;
     }
-
 
     myList.clear();
 
     return 0;
-
 }
