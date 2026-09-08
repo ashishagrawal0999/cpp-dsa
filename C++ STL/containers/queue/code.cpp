24226cf3848398a27b950df5d24b030bf460f41d
ashishagrawal0999 <ashishagrawal09990@gmail.com>
2026-09-08 14:14:16 +0530

    Updated STL Queue File

--- C++ STL/containers/queue/code.cpp
+++ C++ STL/containers/queue/code.cpp
@@ -3,66 +3,94 @@
 
 using namespace std;
 int main(){
-    // Adapter class , provides fifo data structure 
+    // Adapter class , provides fifo data structure
+    // implemented usimng other containers (e.g. deque , list) as the underlying storge
+    // header file -> #include<queue>
+
     // insertion -> rear
     // deletion -> front
 
-    // implemented usimng other containers (e.g. deque , list) as the underlying storge
 
-    // header file -> #include<queue>
-
-    // random access is not allowed
-
-    // queue me iterator nhi hai , front print krke , front se pop krskte hai
-
-    // creation 
+    // creation :
     queue<int> q;
 
-    // push , pop
-
-    // empty , size
-
-    // swap
-
+    // push() : add element to the back of queue
     q.push(10);
     q.push(20);
     q.push(30);
     q.push(40);
+    // 10,20,30,40
 
-    // 10 , 20 , 30 , 40
+    queue<int> q2;
+    q2.push(5);
+    q2.push(6);
+    q2.push(7);
 
-    cout<<q.size()<<" "; // 4
+    // pop() : remove element from the front of queue
+    q.pop();        // 20,30,40
 
-    q.pop();
+    // size() :
+    cout<<q.size()<<" "; // 3
 
-    cout<<q.size()<<" ";
-
-    // 20 , 30 , 40 
-
+    // empty() :
     if (q.empty() == true){
         cout<<"queue is empty"<<endl;
     } else {
         cout<<"queue is not empty"<<endl;
     }
 
-    
 
-    // front , back -> access the first and last element
+    // front() , back() : access the first and last element
+    cout << "Front : " << q.front() << endl;
+    cout << "Back : " << q.back() << endl;
 
-    cout<<"Front"<<q.front()<<endl;
-    cout<<"Back"<<q.back()<<endl;
 
-    
-    queue<int> q2;
-    q2.push(1);
-    q2.push(2);
+    queue<int> temp = q;            // created a queue name temp initialised with the copy of q
+    queue<int> temp2 = q2;          // created a queue name temp2 initialised with the copy of q2
+    // because agar humne pehle he queue ke elemnts print krke remove krdiye toh queue empty hojaayega aur phir swap nhi krpaayenge
 
-    q.swap(q2);
 
-    cout<<q.front()<<" "<<q.back();
+    cout<<"Before swap :"<<endl;
 
-    cout << q2.size() << " ";
+    cout<<"temp :"<<" ";
+    while(!temp.empty()){
+        cout << temp.front() << " ";        // random access is not allowed
+        temp.pop();                         // queue me iterator nhi hai , front print krke pop krskte hai
+    }
     cout<<endl;
-    return 0;
 
+    cout<<"temp2 :"<<" ";
+    while(!temp2.empty()){
+        cout << temp2.front() << " ";
+        temp2.pop();
+    }
+    cout<<endl;
+
+
+
+
+    // swap() :
+    // swapping temp queue
+    temp = q;   // resetting values
+    temp2 = q2;
+
+    temp.swap(temp2);
+    cout << "After swap :" << endl;
+    cout << "temp :" << " ";
+
+    while (!temp.empty()){
+        cout << temp.front() << " ";
+        temp.pop();
+    }
+    cout<<endl;
+
+
+    cout<<"temp2 :"<<" ";
+    while (!temp2.empty()){
+        cout << temp2.front() << " ";
+        temp2.pop();
+    }
+    cout<<endl;
+
+    return 0;
 }
