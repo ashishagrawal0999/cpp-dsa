7fef9cc7e06d361c4c1c323ad5ec0fd85659cf8c
ashishagrawal0999 <ashishagrawal09990@gmail.com>
2026-09-09 13:55:49 +0530

    updated stl deque file

--- C++ STL/containers/deque/code.cpp
+++ C++ STL/containers/deque/code.cpp
@@ -6,21 +6,33 @@ using namespace std;
 int main(){
 
     // Deque -> Doubly ended queue
-    // similar to vectors but allows efficient insertion and removal at both ends
+    // Internally uses segmented/block-based storage
+    // allows efficient insertion and removal at both ends
+    // header file -> #include <deque>
+    // memory : segmented
+
+    // T.C. :
+    // Random access                     : O(1)
+    // push_back() & push_front()        : O(1) amortized
+    // pop_back() & pop_front()          : O(1)
+    // insert() & erase()                : O(n) generally
+    // clear()                           : O(n)
+
+    // deque provides : Random Access Iterator
+    // begin(), end(), rbegin(), rend()
+    // queue is a container adapter and does not provide direct iterators
 
     //       ____________________
     //       __|____|____|____|__
             
     // front  ^                 ^ back
 
+    // It also has functions which are there in vector and list like front , back , begin , end , size etc
+    // similar to vectors but deque ke elements generally contiguous memory mein stored nahi hote, unlike vector
 
-    // insertion and deletion can be done from both ends
-    // header file -> #include <deque>
-    // it also has functions which is there in vector and list like front , back , behin , end , size etc
-    // random access like vector -> T.C. -> O(1)
+    
 
-
-    // creation
+    // creation :
     deque<int> dq;
     dq.push_back(10);       // 10
     dq.push_front(20);      // 20 , 10
@@ -36,33 +48,34 @@ int main(){
     cout<<dq.size()<<endl;
 
     if(dq.empty() == true){
-        cout<<"Deque is empty";
+        cout<<"Deque is empty"<<endl;
     } else {
-        cout<<"Deque is not empty";
+        cout<<"Deque is not empty"<<endl;
     }
 
-    dq.push_back(40);
-    dq.push_front(50);
+    dq.push_back(40);             //  20 , 10 , 40
+    dq.push_front(50);            //  20 , 10 , 40
 
-    cout<<endl;
-
-    cout<<dq[2]<<" "<<dq.at(1)<<endl;
+    cout<<dq[2]<<" "<<dq.at(1)<<endl;       // 10 , 20
 
 
     deque<int>::iterator it =  dq.begin();
     while(it != dq.end()){
-        cout<<*(it)<<" ";
+        cout << *(it) << " ";           // 50 , 20 , 10 , 40
         it++;
     }
+    cout<<endl;
 
-    cout<<dq.size()<<" ";
-    dq.clear();                 // dq is empty
-    cout<<dq.size()<<" ";
+    
+    // insertion at particular index
+    dq.insert(dq.begin(), 90);          // 90 , 50 , 20 , 10 , 40
 
-    dq.insert(dq.begin() , 90);
+    cout<<dq[0]<<" ";                   // 90
 
-    cout<<dq[0]<<" ";
 
+    cout << dq.size() << " ";
+    dq.clear(); // dq is empty
+    cout << dq.size() << " ";
 
     return 0;
 }
